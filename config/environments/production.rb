2fc0e9ed65280943ee1db015f192c44f0523d8ce
Angel Rodriguez <angelr1076@gmail.com>
2018-09-22 17:20:16 -0400

    added AWS S3 bucket access

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,17 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('starshipped'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('USEast'),
+    }
+  }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
