8d369aa79782a28bad4ab2c444a526510c8ca677
Angel Rodriguez <angelr1076@gmail.com>
2018-09-24 13:22:03 -0400

    adjusted hashes on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def all_planets
-        @planet_options = ["Tatooine"],["Naboo"],["Coruscant"],["Bespin"],["Kamino"],["Jakku"],["Corellia"],["Endor"],["Hoth"], ["Kashyyyk"]
+        @planet_options = ["Tatooine", "Tatooine"],["Naboo", "Naboo"],["Coruscant", "Coruscant"],["Bespin", "Bespin"],["Kamino", "Kamino"],["Jakku", "Jakku"],["Corellia", "Corellia"],["Endor", "Endor"],["Hoth", "Hoth"], ["Kashyyyk", "Kashyyyk"]
     end
 end
