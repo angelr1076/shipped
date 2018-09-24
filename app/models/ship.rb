7ceb36361872b072c7700b58fede41f4ae950d79
Angel Rodriguez <angelr1076@gmail.com>
2018-09-24 13:14:56 -0400

    removed aws keys

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -9,6 +9,8 @@ class Ship < ApplicationRecord
 
     validates :name, uniqueness: true, presence: true
 
+    has_one_attached :image
+
     has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
